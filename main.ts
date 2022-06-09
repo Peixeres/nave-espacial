80e61d665b99619a157e7bd8123b86ebf23e08ee
Peixeres <106734363+Peixeres@users.noreply.github.com>
2022-06-09 10:45:35 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,38 +1,71 @@
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-    projectile2 = sprites.createProjectileFromSprite(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . 4 4 . . . . . . . 
-        . . . . . . 4 5 5 4 . . . . . . 
-        . . . . . . 2 5 5 2 . . . . . . 
-        . . . . . . . 2 2 . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
+    projectile = sprites.createProjectileFromSprite(img`
+        ....................e2e22e2e....................
+        .................222eee22e2e222.................
+        ..............222e22e2e22eee22e222..............
+        ...........e22e22eeee2e22e2eeee22e22e...........
+        ........eeee22e22e22e2e22e2e22e22e22eeee........
+        .....222e22e22eeee22e2e22e2e22eeee22e22e222.....
+        ...22eeee22e22e22e22eee22eee22e22e22e22eeee22...
+        4cc22e22e22eeee22e22e2e22e2e22e22eeee22e22e22cc4
+        6c6eee22e22e22e22e22e2e22e2e22e22e22e22e22eee6c6
+        46622e22eeee22e22eeee2e22e2eeee22e22eeee22e22664
+        46622e22e22e22eeee22e2e22e2e22eeee22e22e22e22664
+        4cc22eeee22e22e22e22eee22eee22e22e22e22eeee22cc4
+        6c622e22e22eeee22e22e2e22e2e22e22eeee22e22e226c6
+        466eee22e22e22e22e22e2e22e2e22e22e22e22e22eee664
+        46622e22eeee22e22e22e2e22e2e22e22e22eeee22e22664
+        4cc22e22e22e22e22eeee2e22e2eeee22e22e22e22e22cc4
+        6c622eeee22e22eeee22eee22eee22eeee22e22eeee226c6
+        46622e22e22eeee22e22e2e22e2e22e22eeee22e22e22664
+        466eee22e22e22e22e22e2e22e2e22e22e22e22e22eee664
+        4cc22e22eeee22e22e22e2e22e2e22e22e22eeee22e22cc4
+        6c622e22e22e22e22e22eee22eee22e22e22e22e22e226c6
+        46622eeee22e22e22eeecc6666cceee22e22e22eeee22664
+        46622e22e22e22eeecc6666666666cceee22e22e22e22664
+        4cceee22e22eeecc66666cccccc66666cceee22e22eeecc4
+        6c622e22eeecc66666cc64444446cc66666cceee22e226c6
+        46622e22cc66666cc64444444444446cc66666cc22e22664
+        46622cc6666ccc64444444444444444446ccc6666cc22664
+        4ccc6666ccc6444bcc666666666666ccb4446ccc6666ccc4
+        cccccccc6666666cb44444444444444bc6666666cccccccc
+        64444444444446c444444444444444444c64444444444446
+        66cb444444444cb411111111111111114bc444444444bc66
+        666cccccccccccd166666666666666661dccccccccccc666
+        6666444444444c116eeeeeeeeeeeeee611c4444444446666
+        666e2222222e4c16e4e44e44e44e44ee61c4e2222222e666
+        666eeeeeeeee4c16e4e44e44e44e44ee61c4eeeeeeeee666
+        666eddddddde4c66f4e4effffffe44ee66c4eddddddde666
+        666edffdffde4c66f4effffffffff4ee66c4edffdffde666
+        666edccdccde4c66f4effffffffffeee66c4edccdccde666
+        666eddddddde4c66f4eeeeeeeeeeeeee66c4eddddddde666
+        c66edffdffde4c66e4e44e44e44e44ee66c4edffdffde66c
+        c66edccdccde4c66e4e44e44e44e44ee66c4edccdccde66c
+        cc66666666664c66e4e44e44e44feeee66c46666666666cc
+        .c66444444444c66e4e44e44e44ffffe66c44444444466c.
+        ..c64eee4eee4c66f4e44e44e44f44fe66c4eee4eee46c..
+        ...c4eee4eee4c66f4e44e44e44effee66c4eee4eee4c...
+        ....644444444c66f4e44e44e44e44ee66c444444446....
+        .....64eee444c66f4e44e44e44e44ee66c444eee46.....
+        ......6ccc666c66e4e44e44e44e44ee66c666ccc6......
         `, mySprite, 0, -50)
-    music.buzzer.play()
+    music.pewPew.play()
 })
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
-    AVIÒ.destroy()
-    AVIÒ.destroy()
+    Asteroid.destroy()
+    otherSprite.destroy(effects.fire, 500)
+    info.changeScoreBy(1)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     info.changeLifeBy(-1)
-    mySprite.destroy(effects.fire, 500)
+    otherSprite.destroy(effects.fire, 500)
     scene.cameraShake(4, 500)
 })
-let AVIÒ: Sprite = null
-let projectile2: Sprite = null
+let Asteroid: Sprite = null
+let projectile: Sprite = null
 let mySprite: Sprite = null
 game.splash("BENVINGUTS A L'ESPAI PEPIC", "\"A\" PER COMENÇAR I \"B\" PER DISPARAR")
-effects.clouds.startScreenEffect()
+effects.starField.startScreenEffect()
 mySprite = sprites.create(img`
     ........................
     ........................
@@ -62,26 +95,26 @@ mySprite = sprites.create(img`
 mySprite.setPosition(80, 60)
 controller.moveSprite(mySprite, 100, 100)
 mySprite.setStayInScreen(true)
-info.setLife(2)
-game.onUpdateInterval(1000, function () {
-    AVIÒ = sprites.createProjectileFromSide(img`
-        ....ffffff.........ccc..
-        ....ff22ccf.......cc4f..
-        .....ffccccfff...cc44f..
-        ....cc24442222cccc442f..
-        ...c9b4422222222cc422f..
-        ..c9999b222222222222fc..
-        .c2b991119222222222c22c.
-        c2222b11992222ccccccc22f
-        f222222222222c222ccfffff
-        .f2222222222444222f.....
-        ..ff2222222cf444222f....
-        ....ffffffffff444222c...
-        .........f2cfffc2222c...
-        .........fcc2ffffffff...
-        ..........fc2ffff.......
-        ...........fffff........
-        `, 44, 48)
-    AVIÒ.x += randint(0, scene.screenWidth())
-    AVIÒ.setKind(SpriteKind.Enemy)
+info.setLife(3)
+game.onUpdateInterval(100, function () {
+    Asteroid = sprites.createProjectileFromSide(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 2 1 2 . . . . . . 
+        . . . . . . . 2 1 2 . . . . . . 
+        . . . . . . . 2 1 2 . . . . . . 
+        . . . . . . . 3 1 3 . . . . . . 
+        . . . . . . 2 3 1 3 2 . . . . . 
+        . . . . . . 2 1 1 1 2 . . . . . 
+        . . . . . . 2 1 1 1 3 . . . . . 
+        . . . . . . 3 1 1 1 3 . . . . . 
+        . . . . . . 3 1 1 1 3 . . . . . 
+        . . . . . . 3 1 1 1 3 . . . . . 
+        . . . . . . 2 3 1 3 2 . . . . . 
+        . . . . . . . 2 2 2 . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, 13, 50)
+    Asteroid.x += randint(0, scene.screenWidth())
+    Asteroid.setKind(SpriteKind.Enemy)
 })
